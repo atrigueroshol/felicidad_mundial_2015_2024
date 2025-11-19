42a1bc3af5d43980ae7b21961960cbf48e0ac2e3
Alberto <atriguerosholgado@gmail.com>
2025-11-19 09:26:32 +0100

    Añadimos funcion load

---
+++ src/load.py
@@ -0,0 +1,15 @@
+# FICHERO CON LAS FUNCIONES DE CARGA
+
+#Imports
+import pandas as pd
+
+def load_csvs(lista):
+    dfs = []
+    for csv in lista:
+        try:
+            df = pd.read_csv(csv, sep=";")
+            dfs.append(df)
+        except Exception as e:
+            print(f"ERROR: NO SE HA PODIDO CARGAR {csv}: {e}")
+
+    return dfs
